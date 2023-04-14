1a0a2d065be7d063950b2ff9c0ad7d5bb9ebca75
muraliweb9 <muraliweb@hotmail.com>
2023-04-14 13:23:54 +0100

    Initial

---
+++ src/main/java/com/interview/test/springbootzuul/SpringbootzuulApplication.java
@@ -0,0 +1,13 @@
+package com.interview.test.springbootzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootzuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootzuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/test/springbootzuul/SpringbootzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.test.springbootzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
