cb2bf70245fe6c5232ab74ccef301aac9d17250d
muraliweb9 <muraliweb@hotmail.com>
2023-04-15 07:32:01 +0100

    Initial

--- src/main/java/com/interview/test/springbootzuul/SpringbootzuulApplication.java
+++ src/main/java/com/interview/test/springbootzuul/SpringbootzuulApplication.java
@@ -2,8 +2,11 @@ package com.interview.test.springbootzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZZuulProxy
 public class SpringbootzuulApplication {
 
 	public static void main(String[] args) {
